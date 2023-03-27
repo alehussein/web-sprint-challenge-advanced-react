05dfe8603175e320589c0da48bf6c05746874ca5
Alejandro Hussein <alejandrohussein@Nelmas-MBP.attlocal.net>
2023-03-26 22:41:15 -0500

    hard test

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,63 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import AppFunctional from './AppFunctional';
+import '@testing-library/jest-dom/extend-expect';
+
+
+
+test('renders whithout errors', () => {
+  render(<AppFunctional />);
+});
+
+test('Render Button id ', () => {
+  render(<AppFunctional />);
+
+  const buttonRight = screen.getByText(/RIGHT/i);
+  const buttonLeft = screen.queryByText(/left/i);
+  const buttonUp = screen.queryByText(/up/i);
+  const buttonDown = screen.queryByText(/down/i);
+
+  expect(buttonRight).toBeInTheDocument();
+  expect(buttonLeft).toBeInTheDocument();
+  expect(buttonDown).toBeInTheDocument();
+  expect(buttonUp).toBeInTheDocument();
+
+  expect(buttonRight).toBeTruthy();
+  expect(buttonLeft).toBeTruthy();
+  expect(buttonDown).toBeTruthy();
+  expect(buttonUp).toBeTruthy();
+
+  expect(buttonRight).toHaveTextContent(/right/i);
+  expect(buttonLeft).toHaveTextContent(/left/i);
+  expect(buttonDown).toHaveTextContent(/down/i);
+  expect(buttonUp).toHaveTextContent(/up/i); 
+});
+
+test('render input Label', () =>{
+  render(<AppFunctional />);
+
+  const inputLabel = screen.getByPlaceholderText("Type email");
+  const testEmail = "alejandrohussein1@gmail.com";
+
+  expect(inputLabel).toBeInTheDocument();
+  expect(inputLabel).toBeTruthy();
+
+  fireEvent.change(inputLabel, {target: {value: testEmail}});
+  expect(inputLabel.value).toBe(testEmail)
+});
+
+test('render Submit', () => {
+  render(<AppFunctional />);
+
+  const submit =screen.getByText(/submit/i);
+
+  expect(submit).toBeInTheDocument();
+});
+
+
+
+
+// test('sanity', () => {
+//   expect(true).toBe(false)
+// })
--- frontend/components/Copys/AppClass copy.js
+++ frontend/components/Copys/AppClass copy.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import axios from 'axios';
 
-
+// Suggested initial states
 const initialMessage = ''
 const initialEmail = ''
 const initialSteps = 0
-const initialIndex = 4 
+const initialIndex = 4 // the index the "B" is at
 
-
+// const initialState = {
+//   message: initialMessage,
+//   email: initialEmail,
+//   index: initialIndex,
+//   steps: initialSteps,
+// }
 
 export default class AppClass extends React.Component {
   constructor(props) {
@@ -15,7 +20,7 @@ export default class AppClass extends React.Component {
     this.state ={
       message: initialMessage,
       email: initialEmail,
-      currentIndex: initialIndex,
+      index: initialIndex,
       steps: initialSteps,
     };
     this.URL="http://localhost:9000/api/result";
@@ -27,21 +32,24 @@ export default class AppClass extends React.Component {
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
-  
+  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
+  // You can delete them and build your own logic from scratch.
 
-  getXY = (currentIndex) => {
-    const x = currentIndex % 3 + 1;
-    const y = Math.floor(currentIndex / 3) + 1;
+  getXY = (index) => {
+    const x = index % 3 + 1;
+    const y = Math.floor(index / 3) + 1;
     return {x , y};
+    // It it not necessary to have a state to track the coordinates.
+    // It's enough to know what index the "B" is at, to be able to calculate them.
   }
-  
 
   getXYMessage = () => {
-    const {x, y} = this.getXY(this.state.currentIndex);
-    return `Coordinates (${x}, ${y})`
-    
+    const {x, y} = this.getXY(this.state.initialState);
+    return `Coordinates (${x, y})`
+    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
+    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
+    // returns the fully constructed string.
   }
-  
 
   reset = () => {
     this.setState({
@@ -50,48 +58,38 @@ export default class AppClass extends React.Component {
       message: initialMessage,
       email: initialEmail
     });
-    document.getElementById("email-form").reset();
- 
+    // document.getElementById("email-form").reset();
+    // Use this helper to reset all states to their initial values.
   }
 
   getNextIndex = (direction, currentIndex) => {
     switch(direction){
       case 'left':
         if (currentIndex % 3 === 0) {
-          this.setState({
-            message: "You can't go left"
-          })
           return currentIndex
         }
       return currentIndex - 1
       case 'up':
         if (currentIndex < 3 ){
-          this.setState({
-            message: "You can't go up"
-          })
           return currentIndex
         }
       return currentIndex - 3
       case 'right':
         if ((currentIndex + 1) % 3 === 0 ){
-          this.setState({
-            message: "You can't go right" 
-          });
           return currentIndex
         }
       return currentIndex + 1
       case 'down':
         if (currentIndex > 5) {
-          this.setState({
-            message: "You can't go down"
-          })
           return currentIndex
         }
       return currentIndex + 3
       default:
         return currentIndex 
     }
-   
+    // This helper takes a direction ("left", "up", etc) and calculates what the next index
+    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
+    // this helper should return the current index unchanged.
   }
 
   move = (direction) => {
@@ -102,25 +100,26 @@ export default class AppClass extends React.Component {
         steps: this.state.steps + 1
       })
     }
-   
+    // This event handler can use the helper above to obtain a new index for the "B",
+    // and change any states accordingly.
   }
 
   onChange = (evt) => {
     this.setState({
       email: evt.target.value
     });
-    
+    // You will need this to update the value of the input.
   }
 
   onSubmit = (evt) => {
     evt.preventDefault();
-    const email = this.state.email;
+    const email = evt.target.email.value;
     const { x, y } = this.getXY(this.state.currentIndex);
     const stepss = this.state.steps;
   
     if (x < 1 || x > 3 || y < 1 || y > 3 || stepss < 1 || !email.includes("@")) {
       this.setState({
-        message: "Ouch: email is required."
+        message: "Invalid input"
       });
       return;
     }
@@ -131,19 +130,19 @@ export default class AppClass extends React.Component {
       .then((res) => {
         this.setState({
           message: res.data.message,
-          // steps: initialSteps,
-          // email: initialEmail,
-          // currentIndex: initialIndex
+          steps: initialSteps,
+          email: initialEmail,
+          currentIndex: initialIndex
         });
         document.getElementById("email-form").reset();
       })
       .catch((err) => {
-        // console.error(err);
+        console.error(err);
         this.setState({
-          message: err.response.data.message
+          message: err.message
         });
       });
-  
+    // Use a POST request to send a payload to the server.
   }
 
   render() {
@@ -151,35 +150,30 @@ export default class AppClass extends React.Component {
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">{this.getXYMessage()}</h3>
-          { this.state.steps === 1 ? (
-            <h3 id="steps">{`You moved ${this.state.steps} time`}</h3>
-          ):(
-            <h3 id="steps">{`You moved ${this.state.steps} times`}</h3>
-          )}
-          
+          <h3 id="coordinates">Coordinates (2, 2)</h3>
+          <h3 id="steps">You moved 0 times</h3>
         </div>
         <div id="grid">
           {
             [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === this.state.currentIndex ? ' active' : ''}`}>
-                {idx === this.state.currentIndex ? 'B' : null}
+              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
+                {idx === 4 ? 'B' : null}
               </div>
             ))
           }
         </div>
         <div className="info">
-          <h3 id="message">{this.state.message}</h3>
+          <h3 id="message"></h3>
         </div>
         <div id="keypad">
           <button id="left" onClick={() => this.move("left")}>LEFT</button>
           <button id="up" onClick={() => this.move("up")}>UP</button>
           <button id="right" onClick={() => this.move("right")}>RIGHT</button>
           <button id="down" onClick={() => this.move("down")}>DOWN</button>
-          <button id="reset" onClick={() => this.reset("reset")}>reset</button>
+          <button id="reset" onClick={() => this.move("reset")}>reset</button>
         </div>
-        <form onSubmit={this.onSubmit} id="email-form">
-          <input id="email" type="email" placeholder="type email" name="email" onChange={this.onChange}></input>
+        <form>
+          <input id="email" type="email" placeholder="type email"></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
